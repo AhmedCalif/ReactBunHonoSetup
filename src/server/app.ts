627ffecb8c43058b913d729cad576df364e0361c
Ahmed Calif <ahmedcalif@DESKTOP-3M6LEKG>
2024-11-02 21:20:23 -0700

    update app.ts for render

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,17 +1,22 @@
+import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
-import { expensesRoute } from '../server/routes/expenses'
+import { expensesRoute } from './routes/expenses'
 import { serveStatic } from '@hono/node-server/serve-static'
-import { logger } from 'hono/logger'
 
 const app = new Hono()
 
-
-app.use('*', logger())
-
+// API routes first
 app.route("/api/expenses", expensesRoute)
 
 
 app.use('/*', serveStatic({ root: './dist' }))
 app.get('*', serveStatic({ path: './dist/index.html' }))
 
-export default app
+
+const port = process.env.PORT || 5173
+console.log(`Server starting on port ${port}`)
+
+serve({
+  fetch: app.fetch,
+  port: Number(port)
+})
